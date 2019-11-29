ab9882791a4ed40d7e06cad4331e33d90290db01
Evgeniy <menko.evgeniy@gmail.com>
2019-11-29 18:53:35 +0300

    fill genre and type

---
+++ Final_Project_Disk/sql/fill_catalog.sql
@@ -0,0 +1,17 @@
+USE `panda_disk`;
+INSERT INTO `type` (`type`) VALUES ('Film'),('Game'),('Music');
+
+INSERT INTO `genre`
+(`type_id`,`genre`)
+VALUE
+(1,'Action'),(1,'Westerns'),(1,'Military'),(1,'Detectives')
+,(1,'Document'),(1,'Drama'),(1,'Historical'),(1,'Comedy'),
+(1,'Crime'),(1,'Romance'),(1,'Horror'),(1,'Fantasy'),
+(2,'Action'),(2,'Adventure'),(2,'Arcade'),(2,'Fighting'),(2,'MMORPG'),
+(2,'Quest'),(2,'Racing'),(2,'RPG'),(2,'Shooter'),(2,'Simulator'),
+(2,'Sport'),(2,'Strategy'),
+(3,'Country'),(3,'Blues'),(3,'Jazz'),(3,'Chanson, Romance'),
+(3,'Electronic music'),(3,'Rock'),(3,'Hip Hop'),(3,'Reggae'),(3,'Pop');
+
+
+
